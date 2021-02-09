6d7bdd7c10d0ad1781edf8bc91914d06c51605c1
SamBroberg <SamBroberg@Gmail.com>
2021-02-08 18:45:11 -0600

    making changes

--- composable_pandas/str.py
+++ composable_pandas/str.py
@@ -1,6 +1,6 @@
 from composable import pipeable
 import pandas as pd
-
+import re
 
 @pipeable
 def capitalize(col):
@@ -43,3 +43,266 @@ def capitalize(col):
     
     """
     return col.str.capitalize()
+
+@pipeable
+def decode(encoding, col, error='ignore'):
+    """
+    Help on method decode in module pandas.core.strings:
+
+    decode(encoding, errors='strict') method of pandas.core.strings.StringMethods instance
+    Decode character string in the Series/Index using indicated encoding.
+    Equivalent to :meth:`str.decode` in python2 and :meth:`bytes.decode` in
+    python3.
+    
+    Parameters
+    ----------
+    encoding : str
+    errors : str, optional
+    
+    Returns
+    -------
+    Series or Index
+    """
+    return col.str.decode(encoding, error)
+@pipeable
+def encode(encoding,col,error='ignore'):
+    """
+    Help on method encode in module pandas.core.strings:
+
+    encode(encoding, errors='strict') method of pandas.core.strings.StringMethods instance
+    Encode character string in the Series/Index using indicated encoding.
+    Equivalent to :meth:`str.encode`.
+    
+    Parameters
+    ----------
+    encoding : str
+    errors : str, optional
+    
+    Returns
+    -------
+    encoded : Series/Index of objects
+    """
+    return col.str.encode(encoding,error)
+
+
+@pipeable
+def endswith(pat,col):
+    """
+    Help on method endswith in module pandas.core.strings:
+
+    endswith(pat, na=nan) method of pandas.core.strings.StringMethods instance
+    Test if the end of each string element matches a pattern.
+    
+    Equivalent to :meth:`str.endswith`.
+    
+    Parameters
+    ----------
+    pat : str
+        Character sequence. Regular expressions are not accepted.
+    na : object, default NaN
+        Object shown if element tested is not a string.
+    
+    Returns
+    -------
+    Series or Index of bool
+        A Series of booleans indicating whether the given pattern matches
+        the end of each string element.
+    
+    See Also
+    --------
+    str.endswith : Python standard library string method.
+    str.startswith : Same as endswith, but tests the start of string.
+    str.contains : Tests if string element contains a pattern.
+    
+    Examples
+    --------
+    >>> s = pd.Series(['bat', 'bear', 'caT', np.nan])
+    >>> s
+    0     bat
+    1    bear
+    2     caT
+    3     NaN
+    dtype: object
+    
+    >>> s >> endswith(t)
+    0     True
+    1    False
+    2    False
+    3      NaN
+    dtype: object
+    
+    Specifying `na` to be `False` instead of `NaN`.
+    
+    >>> s >> endswith('t', na=False)
+    0     True
+    1    False
+    2    False
+    3    False
+    dtype: bool
+    """
+    return col.str.endswith(pat)
+@pipeable
+def extract(pat,col,flags=0,expand=True):
+    """
+    Help on method extract in module pandas.core.strings:
+
+    extract(pat, flags=0, expand=True) method of pandas.core.strings.StringMethods instance
+    Extract capture groups in the regex `pat` as columns in a DataFrame.
+    
+    For each subject string in the Series, extract groups from the
+    first match of regular expression `pat`.
+    
+    Parameters
+    ----------
+    pat : str
+        Regular expression pattern with capturing groups.
+    flags : int, default 0 (no flags)
+        Flags from the ``re`` module, e.g. ``re.IGNORECASE``, that
+        modify regular expression matching for things like case,
+        spaces, etc. For more details, see :mod:`re`.
+    expand : bool, default True
+        If True, return DataFrame with one column per capture group.
+        If False, return a Series/Index if there is one capture group
+        or DataFrame if there are multiple capture groups.
+    
+    Returns
+    -------
+    DataFrame or Series or Index
+        A DataFrame with one row for each subject string, and one
+        column for each group. Any capture group names in regular
+        expression pat will be used for column names; otherwise
+        capture group numbers will be used. The dtype of each result
+        column is always object, even when no match is found. If
+        ``expand=False`` and pat has only one capture group, then
+        return a Series (if subject is a Series) or Index (if subject
+        is an Index).
+    
+    See Also
+    --------
+    extractall : Returns all matches (not just the first match).
+    
+    Examples
+    --------
+    A pattern with two groups will return a DataFrame with two columns.
+    Non-matches will be NaN.
+    
+    >>> s = pd.Series(['a1', 'b2', 'c3'])
+    >>> s.str.extract(r'([ab])(\d)')
+         0    1
+    0    a    1
+    1    b    2
+    2  NaN  NaN
+    
+    A pattern may contain optional groups.
+    
+    >>> s.str.extract(r'([ab])?(\d)')
+         0  1
+    0    a  1
+    1    b  2
+    2  NaN  3
+    
+    Named groups will become column names in the result.
+    
+    >>> s.str.extract(r'(?P<letter>[ab])(?P<digit>\d)')
+      letter digit
+    0      a     1
+    1      b     2
+    2    NaN   NaN
+    
+    A pattern with one group will return a DataFrame with one column
+    if expand=True.
+    
+    >>> s.str.extract(r'[ab](\d)', expand=True)
+         0
+    0    1
+    1    2
+    2  NaN
+    
+    A pattern with one group will return a Series if expand=False.
+    
+    >>> s.str.extract(r'[ab](\d)', expand=False)
+    0      1
+    1      2
+    2    NaN
+    dtype: object
+    """
+    return col.str.extract(pat,flags=flags,expand=True)
+
+@pipeable
+def extractall(pat,col,*,flags=0):
+    """
+    Help on method extractall in module pandas.core.strings:
+
+    extractall(pat, flags=0) method of pandas.core.strings.StringMethods instance
+    For each subject string in the Series, extract groups from all
+    matches of regular expression pat. When each subject string in the
+    Series has exactly one match, extractall(pat).xs(0, level='match')
+    is the same as extract(pat).
+    
+    Parameters
+    ----------
+    pat : str
+        Regular expression pattern with capturing groups.
+    flags : int, default 0 (no flags)
+        A ``re`` module flag, for example ``re.IGNORECASE``. These allow
+        to modify regular expression matching for things like case, spaces,
+        etc. Multiple flags can be combined with the bitwise OR operator,
+        for example ``re.IGNORECASE | re.MULTILINE``.
+    
+    Returns
+    -------
+    DataFrame
+        A ``DataFrame`` with one row for each match, and one column for each
+        group. Its rows have a ``MultiIndex`` with first levels that come from
+        the subject ``Series``. The last level is named 'match' and indexes the
+        matches in each item of the ``Series``. Any capture group names in
+        regular expression pat will be used for column names; otherwise capture
+        group numbers will be used.
+    
+    See Also
+    --------
+    extract : Returns first match only (not all matches).
+    
+    Examples
+    --------
+    A pattern with one group will return a DataFrame with one column.
+    Indices with no matches will not appear in the result.
+    
+    >>> s = pd.Series(["a1a2", "b1", "c1"], index=["A", "B", "C"])
+    >>> s.str.extractall(r"[ab](\d)")
+             0
+      match
+    A 0      1
+      1      2
+    B 0      1
+    
+    Capture group names are used for column names of the result.
+    
+    >>> s.str.extractall(r"[ab](?P<digit>\d)")
+            digit
+      match
+    A 0         1
+      1         2
+    B 0         1
+    
+    A pattern with two groups will return a DataFrame with two columns.
+    
+    >>> s.str.extractall(r"(?P<letter>[ab])(?P<digit>\d)")
+            letter digit
+      match
+    A 0          a     1
+      1          a     2
+    B 0          b     1
+    
+    Optional groups that do not match are NaN in the result.
+    
+    >>> s.str.extractall(r"(?P<letter>[ab])?(?P<digit>\d)")
+            letter digit
+      match
+    A 0          a     1
+      1          a     2
+    B 0          b     1
+    C 0        NaN     1
+    """
+    return col.str.extractall(pat,flags = flags)
+
--- tests/test_str.py
+++ tests/test_str.py
@@ -1,10 +1,16 @@
 from composable_pandas.str import capitalize
+from composable_pandas.str import decode
+from composable_pandas.str import encode
+from composable_pandas.str import endswith
+from composable_pandas.str import extract,extractall
 from datetime import datetime
+import re
 
 import numpy as np
 import pytest
 
 from pandas import Series, _testing as tm
+from pandas import DataFrame, Index, MultiIndex, Series, isna, notna
 
 def test_capitalize():
     values = Series(["FOO", "BAR", np.nan, "Blah", "blurg"])
@@ -17,3 +23,78 @@ def test_capitalize():
     mixed = mixed >> capitalize()
     exp = Series(["Foo", np.nan, "Bar", np.nan, np.nan, "Blah", np.nan, np.nan, np.nan])
     tm.assert_almost_equal(mixed, exp)
+
+def test_encode_decode():
+    base = Series(["a", "b", "a\xe4"])
+    series = base >> encode("utf-8")
+
+    f = lambda x: x.decode("utf-8")
+    result = series >> decode("utf-8")
+    exp = series.map(f)
+
+    tm.assert_series_equal(result, exp)
+
+def test_extractall():
+
+    subject_list = [
+        "dave@google.com",
+        "tdhock5@gmail.com",
+        "maudelaperriere@gmail.com",
+        "rob@gmail.com some text steve@gmail.com",
+        "a@b.com some text c@d.com and e@f.com",
+        np.nan,
+        "",
+    ]
+    expected_tuples = [
+        ("dave", "google", "com"),
+        ("tdhock5", "gmail", "com"),
+        ("maudelaperriere", "gmail", "com"),
+        ("rob", "gmail", "com"),
+        ("steve", "gmail", "com"),
+        ("a", "b", "com"),
+        ("c", "d", "com"),
+        ("e", "f", "com"),
+    ]
+    named_pattern = r"""
+    (?P<user>[a-z0-9]+)
+    @
+    (?P<domain>[a-z]+)
+    \.
+    (?P<tld>[a-z]{2,4})
+    """
+    expected_columns = ["user", "domain", "tld"]
+    S = Series(subject_list)
+    # extractall should return a DataFrame with one row for each
+    # match, indexed by the subject from which the match came.
+    expected_index = MultiIndex.from_tuples(
+        [(0, 0), (1, 0), (2, 0), (3, 0), (3, 1), (4, 0), (4, 1), (4, 2)],
+        names=(None, "match"),
+    )
+    expected_df = DataFrame(expected_tuples, expected_index, expected_columns)
+    computed_df = S >> extractall(named_pattern,flags = re.VERBOSE)
+    tm.assert_frame_equal(computed_df, expected_df)
+
+def test_endswith():
+
+    # add category dtype parametrizations for GH-36241
+    values = Series(
+        ["om", "foo_nom", "nom", "bar_foo", "foo"],
+    )
+
+    result = values >> endswith("foo")
+    exp = Series([False, False, False, True, True])
+    tm.assert_series_equal(result, exp)
+
+    result = values >> endswith("foo")
+    exp = Series([False, False, False, True,True])
+    tm.assert_series_equal(result, exp)
+
+def test_extract():
+    # Contains tests like those in test_match and some others.
+    values = Series(["fooBAD__barBAD", np.nan, "foo"])
+    er = [np.nan, np.nan]  # empty row
+
+    result = values >> extract(".*(BAD[_]+).*(BAD)", expand=True)
+    exp = DataFrame([["BAD__", "BAD"], er, er])
+    tm.assert_frame_equal(result, exp)
+
